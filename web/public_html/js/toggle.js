bb4cc7d5132ad3e059be72982cd036963a8b80cf
Sivrat <coffman.travis@gmail.com>
2014-11-04 07:16:21 -0500

    Create toggle.js

---
+++ web/public_html/js/toggle.js
@@ -0,0 +1,16 @@
+//Simple Toggle function for slide menus
+$('#part1').click(function(){
+	$('.lesson1').slideToggle();
+});
+
+$('#part2').click(function(){
+	$('.lesson2').slideToggle();
+});
+
+$('#part3').click(function(){
+	$('.lesson3').slideToggle();
+});
+
+$('#part4').click(function(){
+	$('.lesson4').slideToggle();
+});
